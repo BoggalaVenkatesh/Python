f426834e41421ff1c2a2a4d31c2c3614e6754a14
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2025-05-16 15:55:14 +0530

    python

---
+++ hello.py
@@ -0,0 +1,27 @@
+# print("Hello World")
+
+# Variables
+# 1. Integer
+# 2. Float
+# 3. Strings
+# 4. Boolean
+# 5. list
+# 6. Tuples
+# 7. Distonary
+# 8. Sets
+
+
+# List and tuples both are not same ?
+10 + 20 #values is not execuited but it not stored
+
+res = 10 + 20 # varisable is stroed in res but not printed in the output, you have to print the stored result(res)
+print("Result:", res)
+print(res) # not the res will print in output because we have given the instruction to print 
+# print(10 + 20)
+
+print("Memory Address",id(res)) # returens the momery location, the momory aress of 30 value stored
+# there are methods specific to the data type
+print("Data Type: ",type(res)) # if you want to know the perticuler datatype you wnat to pass the variable
+
+a = 10.23 # decimels are called flooting
+6
