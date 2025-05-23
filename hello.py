4dd0ee92f79a399b1133a2e58d9fe9ae9dbd8215
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2025-05-23 15:36:14 +0530

    python

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@
 # 8. Sets
 
 
-# List and tuples both are not same ?
+""" # List and tuples both are not same ?
 10 + 20 #values is not execuited but it not stored
 
 res = 10 + 20 # varisable is stroed in res but not printed in the output, you have to print the stored result(res)
@@ -21,7 +21,61 @@ print(res) # not the res will print in output because we have given the instruct
 
 print("Memory Address",id(res)) # returens the momery location, the momory aress of 30 value stored
 # there are methods specific to the data type
-print("Data Type: ",type(res)) # if you want to know the perticuler datatype you wnat to pass the variable
+print("Data Type: ",type(res)) # if you want to know the perticuler datatype you wnat to pass the variable """
 
-a = 10.23 # decimels are called flooting
-6
+""" 
+This is block comment
+
+"""
+
+# THis is a line comment
+
+
+# a = 10.23 # decimels are called flooting
+# b = 20.34
+# res = a + b
+# print("Result:", res, "Type: ", type(res))
+
+sample_str = "this a string"
+# print(sample_str, type(sample_str))
+# print(dir(sample_str))
+"""
+'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+"""
+# print(sample_str.capitalize())
+# print(sample_str.casefold())
+# # print(sample_str.center())
+
+# txt = "Hello, And Welcome To My World!"
+# x = txt.casefold()
+# print(x)
+# print(txt.swapcase())
+# print(txt.split(' '))
+# print(txt.find('M'))
+# # python uses zero indexing
+# print(txt.index('H'))
+
+# sample_cher = sample_str[2]
+# print(sample_cher)
+# print(len(sample_str))
+
+a = True
+b = False
+
+print(a and b)# False
+print(a or b)# True
+
+
+a = 2 
+b = 4
+
+print(a + b) # addition
+print(a - b) # substraction
+print(a * b) # multiplication
+print(a / b) # devision
+print(b % a) # modulo division: returns remainder
+print(a // b) # Integer Division
+print(a ** b) # Exponentiantion
+
+
+
