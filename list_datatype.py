8a876938e23562a839589e46f76c570947bbb06b
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2025-05-27 12:28:51 +0530

    datatpye

--- list_datatype.py
+++ list_datatype.py
@@ -2,18 +2,28 @@
 
 # it is a collection values
 
-l = [1, 2, 3, "abc", True, [10, 20, 30, "abc", False]]  #nested list
-print(l, type(l))
+# l = [1, 2, 3, "abc", True, [10, 20, 30, "abc", False]]  #nested list
+# print(l, type(l))
 
-l1 = l[3]
-print(l1)
+# l1 = l[3]
+# print(l1)
 
-l2 = l[5]
-print(l2)
+# l2 = l[5]
+# print(l2)
 
-l3 = l[-1][0]
-print(l3)
+# l3 = l[-1][0]
+# print(l3)
 
-print(l[len(l) - 1][0])
+# print(l[len(l) - 1][0])
 
-print(dir(l))
+# print(dir(l))
+
+'''
+ 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
+ 
+ '''
+l = [10, 20, 30, "abc", False]
+
+# append an element to the end of the list
+
+l.append(True)
